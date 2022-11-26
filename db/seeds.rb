66ff564462d62b8db2d527ba1bdbe5302d1daaca
ibizugbe <abrahamibizugbe@gmail.com>
2022-11-26 12:49:29 +0100

    create  view for greetings

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Greeting.create(text: "Hello John")
+Greeting.create(text: "Hello Peter!")
+Greeting.create(text: "Hello Smith!")
+Greeting.create(text: "Hello Jones!")
+Greeting.create(text: "Hello Candy!")
